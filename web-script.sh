6d2ceb10dfa4b8e596411c14753eef857226f878
W. Eduardo  Valencia <eduardovalencia@Ws-MacBook-Pro.local>
2023-08-26 23:36:14 -0700

    WebScript Changed

--- web-script.sh
+++ web-script.sh
@@ -3,7 +3,9 @@
 sudo yum install httpd php php-fpm php-zip php-gd php-mbstring php-xml php-json -y 
 sudo rm -f /etc/httpd/conf/httpd.conf
 sudo wget -P /etc/httpd/conf/ https://objectstorage.us-phoenix-1.oraclecloud.com/n/bmc-flaviop/b/web-page/o/httpd.conf
-sudo wget -P /var/www/html/ https://objectstorage.us-phoenix-1.oraclecloud.com/n/bmc-flaviop/b/web-page/o/index.php
+sudo wget -P /var/www/html/ https://calc-replica-production.web.app/
+sudo wget -P /var/www/html/ https://calc-replica-production.web.app/css/style.css
+sudo wget -P /var/www/html/ https://calc-replica-production.web.app/js/calc.js
 sudo systemctl start php-fpm
 sudo systemctl enable php-fpm
 sudo setsebool -P httpd_can_network_connect 1
